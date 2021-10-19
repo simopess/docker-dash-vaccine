337fc285237189587c8a6eb6557a10297cd53b9a
Pessina Simone <47665178+simopess@users.noreply.github.com>
2021-10-19 16:09:10 +0200

    Update app.py
    Server..

--- app.py
+++ app.py
@@ -1236,5 +1236,4 @@ def layout():
 app.layout = layout
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
-    # app.run_server(host='0.0.0.0', port=8050, debug=False)
+    app.run_server(host='0.0.0.0', port=8050, debug=False)
