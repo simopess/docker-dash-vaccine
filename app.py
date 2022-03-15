0f12befab4d3394ac3245bff6359b99c4429a355
Pessina Simone <47665178+simopess@users.noreply.github.com>
2022-03-15 23:18:30 +0100

    Update app.py
    Forgot to run server port

--- app.py
+++ app.py
@@ -1242,5 +1242,4 @@ def layout():
 app.layout = layout
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
-    # app.run_server(host='0.0.0.0', port=8050, debug=False)
+    app.run_server(host='0.0.0.0', port=8050, debug=False)
